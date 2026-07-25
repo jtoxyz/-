e1c4ee9a66134ff47fa4be19ed0ff4c63e980e87
jtoxyz <0qb3671k78f477n@gmail.com>
2026-07-25 12:38:30 +0900

    Make payment management page static

--- src/app/admin/payment-management/page.tsx
+++ src/app/admin/payment-management/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-export const runtime = 'edge';
-
 import { useEffect, useMemo, useState } from 'react';
 import AdminNav from '@/components/AdminNav';
 import { useAdminAuth } from '@/hooks/useAdminAuth';
